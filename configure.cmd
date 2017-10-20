f10884dca0025ded2da5f1ef822291ab4c23b02b
KO Myung-Hun <komh@chollian.net>
2017-10-20 22:02:12 +0900

    my: add build scripts for OS/2
        new file:   configure.cmd

---
+++ configure.cmd
@@ -0,0 +1,11 @@
+extproc sh
+
+d=$(dirname "$0" | tr '\\' /)
+
+n=configure
+test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
+
+export LDFLAGS=-Zhigh-mem
+
+opts=""
+"$d/$n" $opts "$@" 2>&1 | tee "$n.log"
